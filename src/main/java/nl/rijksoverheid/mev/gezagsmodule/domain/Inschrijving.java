0bc8ab8e90d2ace92efec31992e1fc7272d66304
Leroy Truong <Leroy.Truong@Logius.nl>
2024-09-03 17:14:45 +0200

    #78 fix: add null defaults when parsing fields from the database

--- src/main/java/nl/rijksoverheid/mev/gezagsmodule/domain/Inschrijving.java
+++ src/main/java/nl/rijksoverheid/mev/gezagsmodule/domain/Inschrijving.java
@@ -37,11 +37,11 @@ public class Inschrijving implements PersoonslijstVeld {
 
     public static Inschrijving from(final Lo3PlRecord lo3PlRecord) {
         Map<String, String> values = new HashMap<>();
-        values.put(DATUM_OPSCHORTING_BIJHOUDING, Objects.toString(lo3PlRecord.getBijhoudingOpschortDatum()));
+        values.put(DATUM_OPSCHORTING_BIJHOUDING, Objects.toString(lo3PlRecord.getBijhoudingOpschortDatum(), null));
         values.put(REDEN_OPSCHORTING_BIJHOUDING, lo3PlRecord.getBijhoudingOpschortReden());
-        values.put(DATUM_VERIFICATIE, Objects.toString(lo3PlRecord.getVerificatieDatum()));
+        values.put(DATUM_VERIFICATIE, Objects.toString(lo3PlRecord.getVerificatieDatum(), null));
         values.put(OMSCHRIJVING_VERIFICATIE, lo3PlRecord.getVerificatieOms());
-        values.put(RNI_DEELNEMER, Objects.toString(lo3PlRecord.getRniDeelnemer()));
+        values.put(RNI_DEELNEMER, Objects.toString(lo3PlRecord.getRniDeelnemer(), null));
 
         return new Inschrijving(values);
     }
