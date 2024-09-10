3d0f459646c36caab9d61f60b4bd9c9204bcde71
Leroy Truong <leroytruong@cupofcode.io>
2024-09-10 12:42:50 +0200

    #88 style: reformat spaces

--- src/test/java/nl/rijksoverheid/mev/gmapi/OpvragenBevoegdheidTotGezagAcceptanceTestMeerderjarige.java
+++ src/test/java/nl/rijksoverheid/mev/gmapi/OpvragenBevoegdheidTotGezagAcceptanceTestMeerderjarige.java
@@ -2,6 +2,7 @@ package nl.rijksoverheid.mev.gmapi;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import nl.rijksoverheid.mev.GezagApplication;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -12,15 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
 import java.util.Set;
 import java.util.stream.Stream;
+
 import nl.rijksoverheid.mev.gezagsmodule.model.Gezagsrelatie;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(
-        classes = {GezagApplication.class, OpenApiGeneratorApplication.class},
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+    classes = {GezagApplication.class, OpenApiGeneratorApplication.class},
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 class OpvragenBevoegdheidTotGezagAcceptanceTestMeerderjarige {
 
@@ -31,16 +35,16 @@ class OpvragenBevoegdheidTotGezagAcceptanceTestMeerderjarige {
 
     static Stream<Arguments> opvragenBevoegdheidTotGezag() {
         return Stream.of(
-                // (input, expected)
-                Arguments.of(
-                        "999998778", // meerderjarige
-                        Set.of(
-                                new Gezagsrelatie("999998778", "999998316", "OG2", "999998778", null, false),
-                                new Gezagsrelatie("999998778", "999999746", "OG1", "999998778", null, false),
-                                new Gezagsrelatie("999998778", "999998341", "OG2", "999998778", null, false),
-                                new Gezagsrelatie("999998778", "999998328", "OG2", "999998778", null, false)
-                        )
+            // (input, expected)
+            Arguments.of(
+                "999998778", // meerderjarige
+                Set.of(
+                    new Gezagsrelatie("999998778", "999998316", "OG2", "999998778", null, false),
+                    new Gezagsrelatie("999998778", "999999746", "OG1", "999998778", null, false),
+                    new Gezagsrelatie("999998778", "999998341", "OG2", "999998778", null, false),
+                    new Gezagsrelatie("999998778", "999998328", "OG2", "999998778", null, false)
                 )
+            )
         );
     }
 
@@ -50,29 +54,29 @@ class OpvragenBevoegdheidTotGezagAcceptanceTestMeerderjarige {
         GezagRequest gezagRequest = new GezagRequest().burgerservicenummer(List.of(input));
 
         webTestClient.post().uri("/api/v1/opvragenBevoegdheidTotGezag")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("OIN", OIN)
-                .bodyValue(gezagRequest)
-                .exchange()
-                .expectStatus().isOk()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody(GezagResponse.class).consumeWith(response -> {
-            GezagResponse result = response.getResponseBody();
-            List<Persoon> personen = new ArrayList<>();
-            GezagTransformer transformer = new GezagTransformer();
-            List<Persoon> expectedPersonen = transformer.fromGezagrelaties(new ArrayList<>(expected));
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("OIN", OIN)
+            .bodyValue(gezagRequest)
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectBody(GezagResponse.class).consumeWith(response -> {
+                GezagResponse result = response.getResponseBody();
+                List<Persoon> personen = new ArrayList<>();
+                GezagTransformer transformer = new GezagTransformer();
+                List<Persoon> expectedPersonen = transformer.fromGezagrelaties(new ArrayList<>(expected));
 
-            Persoon expectedPerson = expectedPersonen.get(0);
-            Persoon actualPerson = result.getPersonen().get(0);
+                Persoon expectedPerson = expectedPersonen.get(0);
+                Persoon actualPerson = result.getPersonen().get(0);
 
-            assertEquals(expectedPerson.getBurgerservicenummer(), actualPerson.getBurgerservicenummer());
-            List<AbstractGezagsrelatie> actualGezagsrelaties = new ArrayList<>(actualPerson.getGezag());
-            for (AbstractGezagsrelatie gezagsrelatie : actualPerson.getGezag()) {
-                assertTrue(expectedPerson.getGezag().contains(gezagsrelatie));
-                actualGezagsrelaties.remove(gezagsrelatie);
-            }
+                assertEquals(expectedPerson.getBurgerservicenummer(), actualPerson.getBurgerservicenummer());
+                List<AbstractGezagsrelatie> actualGezagsrelaties = new ArrayList<>(actualPerson.getGezag());
+                for (AbstractGezagsrelatie gezagsrelatie : actualPerson.getGezag()) {
+                    assertTrue(expectedPerson.getGezag().contains(gezagsrelatie));
+                    actualGezagsrelaties.remove(gezagsrelatie);
+                }
 
-            assertTrue(actualGezagsrelaties.isEmpty());
-        });
+                assertTrue(actualGezagsrelaties.isEmpty());
+            });
     }
 }
