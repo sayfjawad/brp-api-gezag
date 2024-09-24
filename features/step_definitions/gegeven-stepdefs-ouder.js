d9b9f29a0c0b4ec9093a900581b0d0dc6ea2aca7
nkorporaal <nap.korporaal+github@gmail.com>
2024-09-24 14:45:49 +0200

    revert unused parameter

--- features/step_definitions/gegeven-stepdefs-ouder.js
+++ features/step_definitions/gegeven-stepdefs-ouder.js
@@ -1,14 +1,13 @@
 const { Given } = require('@cucumber/cucumber');
 const { createPersoonMetGegevensgroepCollectie,
-    createGegevensgroepCollectie,
-    wijzigGegevensgroep,
-    } = require('./persoon');
+        createGegevensgroepCollectie,
+        wijzigGegevensgroep } = require('./persoon');
 
 Given(/^de persoon met burgerservicenummer '(\d*)' heeft een ouder '(\d)' met de volgende gegevens$/, function (burgerservicenummer, ouderType, dataTable) {
     createPersoonMetGegevensgroepCollectie(this.context, burgerservicenummer, `ouder-${ouderType}`, dataTable);
 });
 
-Given(/^de persoon heeft een ouder '(\d)' met de volgende gegevens$/, function (_, ouderType, dataTable) {
+Given(/^de persoon heeft een ouder '(\d)' met de volgende gegevens$/, function (ouderType, dataTable) {
     createGegevensgroepCollectie(this.context, `ouder-${ouderType}`, dataTable);
 });
 
