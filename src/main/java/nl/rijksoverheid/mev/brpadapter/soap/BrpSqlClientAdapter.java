a5283b0512761f7d86d0431dda78910bde91b180
Patrick Aleman <patrick.aleman@protonmail.com>
2024-12-04 13:40:02 +0100

    verwijderen van ongebruikte properties

--- src/main/java/nl/rijksoverheid/mev/brpadapter/soap/BrpSqlClientAdapter.java
+++ src/main/java/nl/rijksoverheid/mev/brpadapter/soap/BrpSqlClientAdapter.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import java.util.Optional;
 
 @Component
-@Profile("real-brp")
 public class BrpSqlClientAdapter implements BrpClient {
 
     private final PersoonslijstFinder persoonslijstFinder;
