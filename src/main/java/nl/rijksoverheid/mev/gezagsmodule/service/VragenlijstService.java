fbabf1d68a344c3c74881f6dae5cdbcef7aa0c7d
Leroy Truong <leroytruong@cupofcode.io>
2024-12-05 22:29:22 +0100

    260 refactor(sonar): remove unused import

--- src/main/java/nl/rijksoverheid/mev/gezagsmodule/service/VragenlijstService.java
+++ src/main/java/nl/rijksoverheid/mev/gezagsmodule/service/VragenlijstService.java
@@ -1,16 +1,16 @@
 package nl.rijksoverheid.mev.gezagsmodule.service;
 
-import lombok.extern.slf4j.Slf4j;
+import nl.rijksoverheid.mev.exception.HoofdstroomSchemaNietGevondenExceptie;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import nl.rijksoverheid.mev.exception.HoofdstroomSchemaNietGevondenExceptie;
 
 /**
  * Leest de vragen en antwoorden van een CSV-bestand en slaat ze op in een map
