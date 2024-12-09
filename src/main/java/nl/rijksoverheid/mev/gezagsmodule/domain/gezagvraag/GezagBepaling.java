1359011853b9cea4eea6cf977a863da0e9323829
Leroy Truong <leroytruong@cupofcode.io>
2024-12-09 09:27:02 +0100

    260 bug: resolve UnsupportedOperationException caused by mutating immutable collection

--- src/main/java/nl/rijksoverheid/mev/gezagsmodule/domain/gezagvraag/GezagBepaling.java
+++ src/main/java/nl/rijksoverheid/mev/gezagsmodule/domain/gezagvraag/GezagBepaling.java
@@ -116,7 +116,7 @@ public class GezagBepaling {
      * gefiltered)
      */
     public boolean warenVeldenInOnderzoek() {
-        List<String> veldenInOnderzoek = plPersoon.getUsedVeldenInOnderzoek();
+        List<String> veldenInOnderzoek = new ArrayList<>(plPersoon.getUsedVeldenInOnderzoek());
 
         if (plOuder1 != null) veldenInOnderzoek.addAll(plOuder1.getUsedVeldenInOnderzoek());
         if (plOuder2 != null) veldenInOnderzoek.addAll(plOuder2.getUsedVeldenInOnderzoek());
