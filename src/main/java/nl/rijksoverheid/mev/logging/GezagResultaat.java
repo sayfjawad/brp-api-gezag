e706a2e7ee0d3dc66cbf294d4fcada4b155df1ee
Leroy Truong <leroytruong@cupofcode.io>
2025-01-13 00:13:29 +0100

    279 refactor: rewrite `GezagResultaat` as record

--- src/main/java/nl/rijksoverheid/mev/logging/GezagResultaat.java
+++ src/main/java/nl/rijksoverheid/mev/logging/GezagResultaat.java
@@ -1,13 +1,9 @@
 package nl.rijksoverheid.mev.logging;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public final class GezagResultaat {
-    private long plId;
-    private String type;
-    private String toelichting;
-    private String route;
+public record GezagResultaat(
+    long plId,
+    String type,
+    String toelichting,
+    String route
+) {
 }
