e0acee4199d6a8fd2e07d99e27192c42d3ff007a
Patrick Aleman <patrick.aleman@protonmail.com>
2025-01-13 14:54:01 +0100

    refactoren om code complexiteit te verminderen

--- src/main/java/nl/rijksoverheid/mev/gezagsmodule/domain/gezagvraag/ErkenningNa01012023.java
+++ src/main/java/nl/rijksoverheid/mev/gezagsmodule/domain/gezagvraag/ErkenningNa01012023.java
@@ -20,7 +20,6 @@ public class ErkenningNa01012023 extends GezagVraag {
     private static final String V2A_3_VOOR_OUDER2 = "Voor_ouder2";
     private static final String V2A_3_NA = "Na";
     private static final String GESLACHTSAAND_VROUW = "V";
-    private static final String GESLACHTSAAND_MAN = "M";
     private static final int DATE_JAN_1_2023 = 20230101;
 
     protected ErkenningNa01012023(final GezagsBepaling gezagsBepaling) {
@@ -33,66 +32,46 @@ public class ErkenningNa01012023 extends GezagVraag {
         Persoonslijst plPersoon = gezagsBepaling.getPlPersoon();
         Ouder1 persoonOuder1 = plPersoon.getOuder1();
         Ouder2 persoonOuder2 = plPersoon.getOuder2();
+
+        boolean persoonErkend = plPersoon.ongeborenVruchtErkendOfGerechtelijkeVaststelling();
+        boolean persoonOngeborenVruchtErkend = plPersoon.ongeborenVruchtErkend();
+        boolean isPersoonErkend = persoonErkend || persoonOngeborenVruchtErkend;
+
+        if (requirementsForRuleAreMet(persoonOuder1, persoonOuder2, isPersoonErkend)) {
+            isPersoonErkendOpOfNa01012023(isPersoonErkend, persoonOuder1, persoonOuder2);
+            doorWelkeOuderErkend(plPersoon);
+            boolean persoonGeborenVoor01012023 = isPersoonGeborenVoor01012023(persoonErkend, persoonOngeborenVruchtErkend, plPersoon);
+            if (persoonGeborenVoor01012023) {
+                bepaalGezagIncombinatieMetGeboortemoeder(plPersoon, persoonOuder1, persoonOuder2);
+            }
+
+            if (answer != null) {
+                logger.debug("""
+                    2a.3 Erkenning voor of na 1-1-2023?
+                    {}""", answer);
+                gezagsBepaling.getArAntwoordenModel().setV02A03(answer);
+            } else {
+                throw new AfleidingsregelException("Preconditie: vraag 2a.3 - Geboortemoeder niet te bepalen", "Geboortemoeder van bevraagde persoon niet te bepalen");
+            }
+        }
+    }
+
+    private boolean requirementsForRuleAreMet(final Ouder1 persoonOuder1, final Ouder2 persoonOuder2, final boolean isPersoonErkend) {
         if (persoonOuder1 == null || persoonOuder2 == null) {
             String missendeGegeven = (persoonOuder1 == null && persoonOuder2 == null
                 ? "beide ouders van bevraagde persoon" : "een ouder van de bevraagde persoon");
             throw new AfleidingsregelException("Preconditie: vraag 2a.3 - Geen twee ouders bij erkenning", missendeGegeven);
         }
 
-        boolean persoonErkend = plPersoon.ongeborenVruchtErkendOfGerechtelijkeVaststelling();
-        boolean persoonOngeborenVruchtErkend = plPersoon.ongeborenVruchtErkend();
-        boolean isPersoonErkend = persoonErkend || persoonOngeborenVruchtErkend;
         if (isPersoonErkend && persoonOuder1.getDatumIngangFamiliebetrekking() == null) {
             gezagsBepaling.addMissendeGegegevens("datum ingang familie betrekking van ouder 1");
-            return;
+            return false;
         } else if (isPersoonErkend && persoonOuder2.getDatumIngangFamiliebetrekking() == null) {
             gezagsBepaling.addMissendeGegegevens("datum ingang familie betrekking van ouder 2");
-            return;
+            return false;
         }
 
-        isPersoonErkendOpOfNa01012023(isPersoonErkend, persoonOuder1, persoonOuder2);
-        doorWelkeOuderErkend(plPersoon);
-        boolean persoonGeborenVoor01012023 = isPersoonGeborenVoor01012023(persoonOngeborenVruchtErkend, persoonErkend, plPersoon);
-
-        String geslachtsAandOuder1 = persoonOuder1.getGeslachtsAanduiding();
-        String geslachtsAandOuder2 = persoonOuder2.getGeslachtsAanduiding();
-        if(persoonGeborenVoor01012023
-            && GESLACHTSAAND_VROUW.equals(geslachtsAandOuder1)
-            && !GESLACHTSAAND_VROUW.equals(geslachtsAandOuder2)) {
-            answer = V2A_3_VOOR_OUDER1;
-        } else if(persoonGeborenVoor01012023
-            && !GESLACHTSAAND_VROUW.equals(geslachtsAandOuder1)
-            && GESLACHTSAAND_VROUW.equals(geslachtsAandOuder2)) {
-            answer = V2A_3_VOOR_OUDER2;
-        } else if(persoonGeborenVoor01012023
-            && GESLACHTSAAND_VROUW.equals(geslachtsAandOuder1)
-            && GESLACHTSAAND_VROUW.equals(geslachtsAandOuder2)) {
-            String geslachtsnaamPersoon = plPersoon.getPersoon().getGeslachtsnaam();
-            String geslachtsnaamOuder1 = persoonOuder1.getGeslachtsnaam();
-            String geslachtsnaamOuder2 = persoonOuder2.getGeslachtsnaam();
-
-            if(geslachtsnaamPersoon != null
-                && geslachtsnaamPersoon.equals(geslachtsnaamOuder1)
-                && !geslachtsnaamPersoon.equals(geslachtsnaamOuder2)) {
-                answer = V2A_3_VOOR_OUDER1;
-            }
-            if(geslachtsnaamPersoon != null
-                && !geslachtsnaamPersoon.equals(geslachtsnaamOuder1)
-                && geslachtsnaamPersoon.equals(geslachtsnaamOuder2)) {
-                answer = V2A_3_VOOR_OUDER2;
-            }
-        } else if(persoonGeborenVoor01012023) {
-            answer = V2A_3_VOOR;
-        }
-
-        if (answer != null) {
-            logger.debug("""
-                2a.3 Erkenning voor of na 1-1-2023?
-                {}""", answer);
-            gezagsBepaling.getArAntwoordenModel().setV02A03(answer);
-        } else {
-            throw new AfleidingsregelException("Preconditie: vraag 2a.3 - Geboortemoeder niet te bepalen", "Geboortemoeder van bevraagde persoon niet te bepalen");
-        }
+        return true;
     }
 
     private void isPersoonErkendOpOfNa01012023(final boolean isPersoonErkend, final Ouder1 persoonOuder1, final Ouder2 persoonOuder2) {
@@ -116,11 +95,64 @@ public class ErkenningNa01012023 extends GezagVraag {
         }
     }
 
-    private boolean isPersoonGeborenVoor01012023(final boolean persoonOngeborenVruchtErkend, final boolean persoonErkend, final Persoonslijst plPersoon) {
+    private boolean isPersoonGeborenVoor01012023(final boolean persoonErkend, final boolean persoonOngeborenVruchtErkend, final Persoonslijst plPersoon) {
         if (answer == null && !persoonErkend && persoonOngeborenVruchtErkend) {
             return Integer.parseInt(plPersoon.getPersoon().getGeboortedatum()) < DATE_JAN_1_2023;
         }
 
         return false;
+    }
+
+    /**
+     * Geboortemoeder bepaling:
+     * - als 1 van de ouders vrouw is en de andere niet, is de vrouw de moeder
+     * - als beide ouders vrouw zijn, en 1 van de vrouwen heeft dezelfde geslachtsnaam als het kind, is de vrouw de moeder
+     *
+     * @param plPersoon     de persoon
+     * @param persoonOuder1 ouder 1 van de persoon
+     * @param persoonOuder2 ouder 2 van de persoon
+     */
+    private void bepaalGezagIncombinatieMetGeboortemoeder(
+        final Persoonslijst plPersoon,
+        final Ouder1 persoonOuder1,
+        final Ouder2 persoonOuder2) {
+        boolean isOuder1Vrouw = isVrouw(persoonOuder1.getGeslachtsAanduiding());
+        boolean isOuder2Vrouw = isVrouw(persoonOuder2.getGeslachtsAanduiding());
+
+        if(eenVanDeOudersVrouw(isOuder1Vrouw, isOuder2Vrouw)) {
+            answer = isOuder1Vrouw ? V2A_3_VOOR_OUDER1 : V2A_3_VOOR_OUDER2;
+        } else if (beideOudersVrouw(isOuder1Vrouw, isOuder2Vrouw)) {
+            bepaalOpBasisVanGeslachtsNaam(
+                plPersoon.getPersoon().getGeslachtsnaam(),
+                persoonOuder1.getGeslachtsnaam(),
+                persoonOuder2.getGeslachtsnaam());
+        } else {
+            answer = V2A_3_VOOR;
+        }
+    }
+
+    private boolean isVrouw(final String geslachtsAand) {
+        return GESLACHTSAAND_VROUW.equals(geslachtsAand);
+    }
+
+    private boolean eenVanDeOudersVrouw(final boolean isOuder1Vrouw, final boolean isOuder2Vrouw) {
+        return isOuder1Vrouw ^ isOuder2Vrouw;
+    }
+
+    private boolean beideOudersVrouw(final boolean isOuder1Vrouw, final boolean isOuder2Vrouw) {
+        return isOuder1Vrouw && isOuder2Vrouw;
+    }
+
+    private void bepaalOpBasisVanGeslachtsNaam(
+        final String geslachtsnaam, final String geslachtsnaamOuder1, final String geslachtsnaamOuder2) {
+        if (geslachtsnaam != null &&
+            geslachtsnaam.equals(geslachtsnaamOuder1)
+            && !geslachtsnaam.equals(geslachtsnaamOuder2)) {
+            answer = V2A_3_VOOR_OUDER1;
+        } else if (geslachtsnaam != null &&
+            !geslachtsnaam.equals(geslachtsnaamOuder1)
+            && geslachtsnaam.equals(geslachtsnaamOuder2)) {
+            answer = V2A_3_VOOR_OUDER2;
+        }
     }
 }
